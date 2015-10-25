840e0bc922a1ea8b86e3a677c0d6ba9eb36c4ea2
misasagi <kosmos@misasagideMacBook-Air.local>
2015-10-25 19:52:58 +0800

    debug

--- main.go
+++ main.go
@@ -2,76 +2,21 @@ package main
 
 import(
 	"fmt"
+	_"./schema/node"
 )
 
-type value interface{}
 
-type variable struct{
-	variableValue value
-	typeFlag uint8
-}
-
-type listNode struct{
-	nodeValue *variable
-	nextNode *listNode
-}
-
-type list struct{
-	fristNode *listNode
-}
-
-func (p *list)getLength()int{
-	len := 0
-	if(p.fristNode != nil){
-		len += 1
-		pointer := p.fristNode
-		for(pointer.nextNode != nil){
-			len += 1
-		}
-	}
-	return len
-}
-
-func (p *list)getLastNode()*listNode{
-	pointer := p.fristNode
-	for(pointer.nextNode != nil){
-		pointer = pointer.nextNode
-	}
-	return pointer
-}
-
-
-
-func (p *list)concat(node *listNode){
-	p.getLastNode().nextNode = node
-}
-func (p *list)String()string{
-	if(p.getLength() == 0){
-		return fmt.Sprintf("nil")
-	}else{
-		pointer := p.fristNode
-		return sprintf(pointer)
+func reader(expr string)(func()(byte)){
+	rest := expr
+	return func()(byte){
+		fmt.Println(rest[0])
+		return rest[0]
 	}
 }
-
-func sprintf(pointer *listNode)string{
-	if(pointer != nil){
-		return fmt.Sprintf("%v -> %s",pointer.nodeValue,sprintf(pointer.nextNode))
-	}else{
-		return "nil"
-	}
-}
-
-
 
 func main(){
-	n1 := &listNode{nodeValue:&variable{233,0}}
-	l := &list{n1}
-	*n1.nextNode = listNode{nodeValue:&variable{233,0}}
-	fmt.Println(l.getLength());
-	fmt.Println(l);
-	fmt.Println(l.fristNode.nodeValue.variableValue)
+	expr := "(head (body (p 233)) 233)"
+	nreader := reader(expr)
+	nreader()
 	fmt.Println(233)
 }
-
-
